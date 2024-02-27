dc97c5afb835ae8eb41971e1d47a487e6e7e5729
Tima Baisalov <baisalov.t98@gmail.com>
2024-02-27 10:02:48 +0600

    components are created

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All pizzas</li>
+        <li>Meat Lover's Pizza</li>
+        <li>Veggie Pizza</li>
+        <li>BBQ Chicken Pizza</li>
+        <li>Mushroom Pizza</li>
+        <li>Sausage Pizza</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
