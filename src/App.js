cabf4ddc5a28fe747db34157aeedda55b1105c0a
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-01 06:17:02 +0600

    skeleton loader added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
 function App() {
-  const [allData, setAllData] = useState([])
+  const [allData, setAllData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   React.useEffect(() => {
-    fetch('https://65e0e5e8d3db23f7624a4181.mockapi.io/items')
-    .then(res => res.json())
-    .then(data => setAllData(data))
+    fetch("https://65e0e5e8d3db23f7624a4181.mockapi.io/items")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllData(data);
+        setIsLoading(false);
+      })
+      
   }, []);
-  
 
-  console.log(allData)
+  console.log(allData);
 
   return (
     <div className="App">
@@ -28,9 +33,10 @@ function App() {
             </div>
             <h2 className="content__title">All Pizzas</h2>
             <div className="content__items">
-              {allData.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
+             
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+                : allData.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
             </div>
           </div>
         </div>
